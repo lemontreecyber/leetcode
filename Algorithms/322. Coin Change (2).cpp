5072519df7ec3ec7c52e77ae033898eab298eb4f
lemontreecyber <73319613+lemontreecyber@users.noreply.github.com>
2020-12-08 10:16:47 -0800

    Create 322. Coin Change (2).cpp

---
+++ Algorithms/322. Coin Change (2).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        // if (amount <= 0) return (amount == 0) ? 0 : -1;
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int const c : coins)
+            for (int i = c; i <= amount; ++i) {
+                if (dp[i - c] == INT_MAX) continue;
+                dp[i] = min(dp[i], dp[i - c] + 1);
+            }
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
